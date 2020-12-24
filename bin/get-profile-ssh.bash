ab92ad7d761d12919be1a2aa15bb64ad129ff255
Philip Hadviger <philip.hadviger@phad.me>
2020-12-24 13:21:30 +0900

    fix spacing

--- bin/get-profile-ssh.bash
+++ bin/get-profile-ssh.bash
@@ -9,49 +9,49 @@ IFS=$'\n\t'
 
 # obtain folderid from bitwarden based on MY_PROFILE
 FOLDER_ID="$( \
-  bw get folder "profile-${MY_PROFILE}" \
-  | jq -rM '.id' \
-  )"
+	bw get folder "profile-${MY_PROFILE}" \
+	| jq -rM '.id' \
+	)"
 
 if [[ -z "${FOLDER_ID}" ]]; then
-  >&2 echo ":: unable to obtain folderid from bitwarden"
-  exit 1
+	>&2 echo ":: unable to obtain folderid from bitwarden"
+	exit 1
 fi
 
 if ! bw unlock --check > /dev/null 2>&1; then
-  >&2 echo ":: bitwarden profile is locked"
+	>&2 echo ":: bitwarden profile is locked"
 	exit 1
 fi
 
 # 1. obtain array of ssh keys from bitwarden
 # 2. put each item as a base64 encoded string on a separate line
 DATA="$( \
-  bw list items \
-  --folderid "${FOLDER_ID}" \
-  | jq -rM --from-file <(cat <<- DOC
-    map(
-      select(.fields)
-      | .fields
-      | from_entries
-      | select(.type == "ssh")
-    )
-    | .[]
-    | @base64'
+	bw list items \
+	--folderid "${FOLDER_ID}" \
+	| jq -rM --from-file <(cat <<- DOC
+		map(
+			select(.fields)
+			| .fields
+			| from_entries
+			| select(.type == "ssh")
+		)
+		| .[]
+		| @base64'
 DOC
 ))"
 
 for ROW in $(echo "${DATA}"); do
-  _jq() {
-    echo ${ROW} | base64 --decode | jq -r ${1}
-  }
+	_jq() {
+		echo ${ROW} | base64 --decode | jq -r ${1}
+	}
 
-  PRIVATE="$(_jq '.private' | base64 --decode)"
-  PUBLIC="$(_jq '.public' | base64 --decode)"
-  KEY_FILE="${HOME}/.ssh/$(_jq '.name')"
+	PRIVATE="$(_jq '.private' | base64 --decode)"
+	PUBLIC="$(_jq '.public' | base64 --decode)"
+	KEY_FILE="${HOME}/.ssh/$(_jq '.name')"
 
-  echo "${PUBLIC}" > "${KEY_FILE}.pub"
-  echo "${PRIVATE}" > "${KEY_FILE}"
-  chmod 400 "${KEY_FILE}.pub" "${KEY_FILE}"
+	echo "${PUBLIC}" > "${KEY_FILE}.pub"
+	echo "${PRIVATE}" > "${KEY_FILE}"
+	chmod 400 "${KEY_FILE}.pub" "${KEY_FILE}"
 done
 
 
