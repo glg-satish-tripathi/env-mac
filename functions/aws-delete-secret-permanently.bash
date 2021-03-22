1f6198ed38df25542d7e1200d016e8fb03ee8225
Philip Hadviger <philip.hadviger@phad.me>
2021-03-22 10:20:28 +0900

    remove formatting

--- functions/aws-delete-secret-permanently.bash
+++ functions/aws-delete-secret-permanently.bash
@@ -1,10 +1,8 @@
-# vi: noet :
-# noet because we need tabs for the heredoc
 aws-delete-secret-permanently () {
-	local REGION NAME
-	REGION="${1}"
-	NAME="${2}"
-	aws --region "${REGION}" secretsmanager restore-secret --secret-id "${NAME}"
-	aws --region "${REGION}" secretsmanager delete-secret --force-delete-without-recovery --secret-id "${NAME}"
-	echo "aws --region '${REGION}' secretsmanager get-secret-value --secret-id '${NAME}'"
+local REGION NAME
+REGION="${1}"
+NAME="${2}"
+aws --region "${REGION}" secretsmanager restore-secret --secret-id "${NAME}"
+aws --region "${REGION}" secretsmanager delete-secret --force-delete-without-recovery --secret-id "${NAME}"
+echo "aws --region '${REGION}' secretsmanager get-secret-value --secret-id '${NAME}'"
 }
