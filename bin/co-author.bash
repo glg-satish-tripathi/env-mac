9af6a8659081e59468b29f203448ab3ee1b01267
Philip Hadviger <613305+datfinesoul@users.noreply.github.com>
2021-05-05 08:46:27 +0900

    git config.template hack

--- bin/co-author.bash
+++ bin/co-author.bash
@@ -8,13 +8,21 @@ set -o pipefail
 IFS=$'\n\t'
 
 TYPE="$(git rev-parse HEAD -- &> /dev/null && echo "git" || echo "none")"
+# This check has the following purpose
+# - Make sure that this is a git repo
+# - Make sure that the repo has had a previous commit
+# - It uses my git cs alias, because I want the use of this to be intentional
 if [[ "${TYPE}" == "git" ]]; then
 	TEMPFILE="$(mktemp /tmp/co-author.XXXXXXXX)"
-	git shortlog --summary --numbered --email --all \
-		| cut -f2- \
-		| awk '$0="Co-authored-by: "$0' \
-		| fgrep -v "$(git config user.email)" \
-		| fzf --multi --exit-0 \
+	cat \
+		"$(git config commit.template)" \
+		<( \
+			git shortlog --summary --numbered --email --all \
+			| cut -f2- \
+			| awk '$0="Co-authored-by: "$0' \
+			| fgrep -v "$(git config user.email)" \
+			| fzf --multi --exit-0 \
+		) \
 		> "${TEMPFILE}"
 	# TODO: potentially add commit.template to the top later?
 	git cs --template="${TEMPFILE}" "$@"
