aedbbe0ee40561d6dd70e4d47ab245c88373b455
Philip Hadviger <613305+datfinesoul@users.noreply.github.com>
2021-07-05 06:59:37 -0400

    some more new machine setup stuff

---
+++ bashrc-apply.bash
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+SCRIPT="$(readlink -e -- "${0}")"
+SCRIPT_DIR="$(dirname "${SCRIPT}")"
+
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -o errexit
+set -o nounset
+set -o pipefail
+IFS=$'\n\t'
+
+if [[ "${SCRIPT_DIR}" != "$(readlink -e -- "$(pwd)")" ]]; then
+  echo "please execute this script from its own directory"
+  exit 1
+fi
+
+if [[ "$(id -u)" -eq "0" ]]; then
+  echo "please DO NOT run as root";
+  exit 1
+fi
+
+function cleanup {
+  :
+}
+trap cleanup EXIT
+
+LOG="/tmp/bash-apply.bash.log"
+
+# log stdout/stderr to a file and stdout
+exec &> >(tee "${LOG}")
+
+for FILE in bashrc/*.bash; do
+  FILE_NAME="$(basename "${FILE}")"
+  # remove section in .bashrc ( eg. #:somefile.bash:[+-] )
+  sed -i '/#:'"${FILE_NAME}"':[+]/,/#:'"${FILE_NAME}"':[-]/d' "${HOME}/.bashrc"
+  # add the section back
+  {
+    echo "#:${FILE_NAME}:+"
+    cat "${FILE}"
+    echo "#:${FILE_NAME}:-"
+  } >> "${HOME}/.bashrc"
+done
