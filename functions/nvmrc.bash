021fe8a764f7c6f3144abc33d4b8576eb4df9423
Philip Hadviger <613305+datfinesoul@users.noreply.github.com>
2022-02-01 20:47:02 +0900

    deal with v16 and v16.13 types of versions

--- functions/nvmrc.bash
+++ functions/nvmrc.bash
@@ -1,10 +1,20 @@
 nvmrc () {
   # swtich to the nodejs version automatically, if an .nvmrc file is found in the current dir.
   [[ -f .nvmrc ]] || return 0
-  local CURRENT_VERSION="$(node -v)"
-  local DESIRED_VERSION="$(<.nvmrc)"
-  if [[ "${CURRENT_VERSION//[$'\r\t\n v']}" != "${DESIRED_VERSION//[$'\r\t\n v']}" ]]; then
-    nvm use "${DESIRED_VERSION//[$'\r\t\n v']}"
-    echo
+  local CURRENT DESIRED
+  CURRENT="$(node -v)"
+  CURRENT="${CURRENT//[$'\r\t\n v']}"
+  DESIRED="$(<.nvmrc)"
+  DESIRED="${DESIRED//[$'\r\t\n v']}"
+  if [[ -z "${CURRENT}" || -z ${DESIRED} ]]; then
+    return 0
+  fi
+  if [[ \
+    "${DESIRED}" != "${CURRENT}" \
+    && "${DESIRED}" != "${CURRENT%.*}" \
+    && "${DESIRED}" != "${CURRENT%%.*}" \
+    ]]; then
+      nvm use "${DESIRED//[$'\r\t\n v']}"
+      echo
   fi
 }
