e5a286d533e8928ca74bb02d240dc780bda1abce
Philip Hadviger <613305+datfinesoul@users.noreply.github.com>
2022-03-07 14:54:02 +0900

    more installer fixes

--- bashrc/path-env-ubuntu-bin.bash
+++ bashrc/path-env-ubuntu-bin.bash
@@ -1,4 +1,4 @@
 # display the default GOPATH
 # go env GOPATH
-export PATH="${HOME}/env-ubuntu/bin:/usr/local/go/bin:${HOME}/go/bin:${PATH}"
+export PATH="${HOME}/env-ubuntu/bin:${HOME}/.local/bin:/usr/local/go/bin:${HOME}/go/bin:${PATH}"
 export PATH="/usr/local/sbin:${PATH}"
