540457f0dbfa624d5a8db584c637a10e77cdcd3f
Satish Tripathi <111346255+glg-satish-tripathi@users.noreply.github.com>
2024-06-19 17:00:47 +0530

    Update aws-clear-env.bash

--- functions/aws-clear-env.bash
+++ functions/aws-clear-env.bash
@@ -1,7 +1,8 @@
 aws-clear-env() {
-  local VARS
-  #mapfile -t VARS < <(env | awk -F'=' '/^AWS_/ {print $1}')
-  IFS=$'\n' read -r -d '' -a VARS < <(env | awk -F'=' '/^AWS_/ {print $1}')
+  VARS=()
+  while IFS= read -r line; do
+    VARS+=("$line")
+  done < <(env | awk -F'=' '/^AWS_/ {print $1}')
   unset "${VARS[@]}"
   unset HTTP_PROXY HTTPS_PROXY
 }
